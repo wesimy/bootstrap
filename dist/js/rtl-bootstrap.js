97127795d61a08073f07333770d5f17d9b4bdd28
Moataz <wesimy@gmail.com>
2017-02-25 04:51:39 +0400

    added direction var

--- dist/js/rtl-bootstrap.js
+++ dist/js/rtl-bootstrap.js
@@ -1,6 +1,6 @@
 /*!
- * Bootstrap v4.0.0-alpha.6 (https://getbootstrap.com)
- * Copyright 2011-2017 The Bootstrap Authors (https://github.com/twbs/bootstrap/graphs/contributors)
+ * Bootstrap v4.0.0-alpha.6 (https://getrtlbootstrap.com)
+ * Copyright 2011-2017 Moataz Elwesimy
  * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
  */
 
